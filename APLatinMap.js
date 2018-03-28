0c14e3c86766d2120057156aa35072cbab1f02b6
Drew Wadsworth <drew.wadsworth@gmail.com>
2018-03-27 21:08:13 -0700

    Organized code and adding De Bello code

--- APLatinMap.js
+++ APLatinMap.js
@@ -2,13 +2,20 @@ let canvas = document.getElementById("canvas");
 let next = document.getElementById("nextButton");
 let previous = document.getElementById("previousButton");
 let aeneid = document.getElementById("aeneidButton");
+let deBello = document.getElementById("deBelloButton");
 let ctx = canvas.getContext("2d");
 let aeneidMap = null;
 let currentLoc = null;
+const aeUnFocused = "#7344f4";	//Purple
+const dbUnFocused = "red";		//Red
+const focused = "black";		//Black
+const xCol = 0;
+const yCol = 1;
+const starRadius = 8;
 
 canvas.addEventListener("mousedown", mouseClick, false);
 
-aeLoc = [
+const aeLoc = [
 	[579, 284],
 	[575, 268],
 	[555, 336],
@@ -24,7 +31,7 @@ aeLoc = [
 	[380, 252]
 ];
 
-aeDes = [
+const aeDes = [
 	"Troy",
 	"Point 2",
 	"Point 3",
@@ -39,55 +46,113 @@ aeDes = [
 	"Point 12",
 	"Point 13"];
 
+const dbLoc = [
+	[362, 205],
+	[226, 85],
+	[247, 163]
+];
+
+const dbDes = [
+	"Mutina?",
+	"Britanni",
+	"Avaricum"
+];
+
 initialPrintMap();
 
-aeneid.onclick = function(){
-	aeneidMap = true;
+aeneid.onclick = function()
+{
+	if (!aeneidMap)
+	{
+		resetMap();
 
-	ctx.beginPath();
-	ctx.moveTo(aeLoc[0][0], aeLoc[0][1]);
+		aeneidMap = true;
 
-	for (var i = 1; i < aeLoc.length; i++) {
-			ctx.lineTo(aeLoc[i][0], aeLoc[i][1]);
-	}
-	ctx.stroke();
+		ctx.beginPath();
+		ctx.moveTo(aeLoc[0][xCol], aeLoc[0][yCol]);
 
-	ctx.fillStyle = "#7344f4";
+		for (var i = 1; i < aeLoc.length; i++)
+		{
+			ctx.lineTo(aeLoc[i][xCol], aeLoc[i][yCol]);
+		}
 
-	for (var i = 0; i < aeLoc.length; i++) {
-		star(ctx, aeLoc[i][0], aeLoc[i][1]);
+		ctx.stroke();
+
+		initialPrintMap();
 	}
 }
 
-previous.onclick = function(){
+deBello.onclick = function()
+{
+	if (aeneidMap || aeneidMap == null)
+	{
+		resetMap();
+
+		aeneidMap = false;
+
+		ctx.beginPath();
+		ctx.moveTo(dbLoc[0][xCol], dbLoc[0][yCol]);
+
+		for (var i = 1; i < dbLoc.length; i++)
+		{
+			ctx.lineTo(dbLoc[i][xCol], dbLoc[i][yCol]);
+		}
+
+		ctx.stroke();
+
+		initialPrintMap();
+	}
+}
+
+previous.onclick = function()
+{
 	if (currentLoc == 0 || currentLoc == null)
 	{
 		return;
 	}
 	if (aeneidMap)
 	{
-		ctx.fillStyle = "#7344f4";
-		star(ctx, aeLoc[currentLoc][0], aeLoc[currentLoc][1])
+		ctx.fillStyle = aeUnFocused;
+		star(ctx, aeLoc[currentLoc][xCol], aeLoc[currentLoc][yCol])
 		currentLoc -= 1;
-		ctx.fillStyle = "black";
-		star(ctx, aeLoc[currentLoc][0], aeLoc[currentLoc][1])
+		ctx.fillStyle = focused;
+		star(ctx, aeLoc[currentLoc][xCol], aeLoc[currentLoc][yCol])
+		displayDescription(currentLoc);
+	}
+	else if (aeneidMap != null)
+	{
+		ctx.fillStyle = dbUnFocused;
+		star(ctx, dbLoc[currentLoc][xCol], dbLoc[currentLoc][yCol])
+		currentLoc -= 1;
+		ctx.fillStyle = focused;
+		star(ctx, dbLoc[currentLoc][xCol], dbLoc[currentLoc][yCol])
 		displayDescription(currentLoc);
 	}
 }
 
-next.onclick = function(){
-	if (currentLoc == aeLoc.length - 1 || currentLoc == null)
+next.onclick = function()
+{
+	if (aeneidMap && currentLoc == aeLoc.length - 1 || !aeneidMap && currentLoc == dbLoc.length - 1 || currentLoc == null)
 	{
 		return;
 	}
 
 	if (aeneidMap)
 	{
-		ctx.fillStyle = "#7344f4";
-		star(ctx, aeLoc[currentLoc][0], aeLoc[currentLoc][1])
+		ctx.fillStyle = aeUnFocused;
+		star(ctx, aeLoc[currentLoc][xCol], aeLoc[currentLoc][yCol])
 		currentLoc += 1;
-		ctx.fillStyle = "black";
-		star(ctx, aeLoc[currentLoc][0], aeLoc[currentLoc][1])
+		ctx.fillStyle = focused;
+		star(ctx, aeLoc[currentLoc][xCol], aeLoc[currentLoc][yCol])
+		displayDescription(currentLoc);
+	}
+	else if (aeneidMap != null)
+	{
+		ctx.fillStyle = dbUnFocused;
+		star(ctx, dbLoc[currentLoc][xCol], dbLoc[currentLoc][yCol])
+		currentLoc += 1;
+		ctx.fillStyle = focused;
+		star(ctx, dbLoc[currentLoc][xCol], dbLoc[currentLoc][yCol])
 		displayDescription(currentLoc);
 	}
 }
@@ -97,20 +162,42 @@ function mouseClick(event)
 	let x = event.x - canvas.offsetLeft;
 	let y = event.y - canvas.offsetTop;
 
-	for (var i = 0; i < aeLoc.length; i++) {
-		if (x >= aeLoc[i][0] - 8 && x <= aeLoc[i][0] + 8 && y >= aeLoc[i][1] - 8 && y <= aeLoc[i][1] + 8)	{
-			if (currentLoc != null)
+	if (aeneidMap)
+	{
+		for (var i = 0; i < aeLoc.length; i++)
+		{
+			if (x >= aeLoc[i][xCol] - starRadius && x <= aeLoc[i][xCol] + starRadius && y >= aeLoc[i][yCol] - starRadius && y <= aeLoc[i][yCol] + starRadius)
 			{
-				ctx.fillStyle = "#7344f4";
-				star(ctx, aeLoc[currentLoc][0], aeLoc[currentLoc][1])
+				if (currentLoc != null)
+				{
+					ctx.fillStyle = aeUnFocused;
+					star(ctx, aeLoc[currentLoc][xCol], aeLoc[currentLoc][yCol])
+				}
+				currentLoc = i;
+				ctx.fillStyle = focused;
+				star(ctx, aeLoc[i][xCol], aeLoc[i][yCol])
+				displayDescription(currentLoc);
 			}
-			currentLoc = i;
-			ctx.fillStyle = "black";
-			star(ctx, aeLoc[i][0], aeLoc[i][1])
-			displayDescription(currentLoc);
 		}
 	}
-	//ctx.clearRect(0, 0, canvas.width, canvas.height)
+	else if (aeneidMap != null)
+	{
+		for (var i = 0; i < dbLoc.length; i++)
+		{
+			if (x >= dbLoc[i][xCol] - starRadius && x <= dbLoc[i][xCol] + starRadius && y >= dbLoc[i][yCol] - starRadius && y <= dbLoc[i][yCol] + starRadius)
+			{
+				if (currentLoc != null)
+				{
+					ctx.fillStyle = dbUnFocused;
+					star(ctx, dbLoc[currentLoc][xCol], dbLoc[currentLoc][yCol])
+				}
+				currentLoc = i;
+				ctx.fillStyle = focused;
+				star(ctx, dbLoc[i][xCol], dbLoc[i][yCol])
+				displayDescription(currentLoc);
+			}
+		}
+	}
 
 	//alert("x:" + x + " y:" + y);
 }
@@ -120,7 +207,7 @@ function mouseClick(event)
 */
 function star(ctx, x, y)
 {
-	radius = 8;
+	radius = starRadius;
 	points = 5;
 	insetFraction = .5;
 	ctx.save();
@@ -142,12 +229,31 @@ function star(ctx, x, y)
 //Prints initial stars on map
 function initialPrintMap()
 {
-	ctx.fillStyle = "#7344f4";	//Purple
+	ctx.fillStyle = aeUnFocused;
 
 	//Aeneid star map markers
-	for (var i = 0; i < aeLoc.length; i++) {
-		star(ctx, aeLoc[i][0], aeLoc[i][1]);
+	for (var i = 0; i < aeLoc.length; i++)
+	{
+		star(ctx, aeLoc[i][xCol], aeLoc[i][yCol]);
 	}
+
+	ctx.fillStyle = dbUnFocused;
+
+	//De Bello star map markers
+	for (var i = 0; i < dbLoc.length; i++)
+	{
+		star(ctx, dbLoc[i][xCol], dbLoc[i][yCol]);
+	}
+}
+
+//Resets webpage to initial state
+function resetMap()
+{
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	aeneidMap = null;
+	currentLoc = null;
+	displayDescription(null);
+	initialPrintMap();
 }
 
 /*	Sets description element below canvas to tell about the
@@ -156,5 +262,20 @@ function initialPrintMap()
 function displayDescription(location)
 {
 	let description = document.getElementById("description");
-	description.innerText = aeDes[location];
+
+	if (location != null)
+	{
+		if (aeneidMap)
+		{
+			description.innerText = aeDes[location];
+		}
+		else if (aeneidMap != null)
+		{
+			description.innerText = dbDes[location];
+		}
+	}
+	else
+	{
+		description.innerText = "";
+	}
 }
